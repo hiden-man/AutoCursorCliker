e55001a276d8cd8a3e2bdac6e1435134e30b4346
Hidenman <elderlutique@gmail.com>
2023-01-22 19:56:23 +0200

    Add project files.

---
+++ AutoCursorCliker/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace AutoCursorCliker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(278, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.Gold;
+            this.textBox1.Location = new System.Drawing.Point(142, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(2, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Кількість натискань:";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 114);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.ForeColor = System.Drawing.Color.Gold;
+            this.textBox3.Location = new System.Drawing.Point(164, 87);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(106, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.Color.Gold;
+            this.textBox2.Location = new System.Drawing.Point(142, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(86, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Magenta;
+            this.button2.Location = new System.Drawing.Point(5, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 30);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Запуск";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(161, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "час до запуску:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(2, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Інтервал між\r\nнатисканням (.млс):";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.Gold;
+            this.radioButton1.Location = new System.Drawing.Point(24, 6);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 30);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ліва\r\nкнопка";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.Gold;
+            this.radioButton2.Location = new System.Drawing.Point(98, 6);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 30);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Права\r\nкнопка";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.ForeColor = System.Drawing.Color.Gold;
+            this.radioButton3.Location = new System.Drawing.Point(165, 6);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(84, 30);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Подвійне\r\nнатискання";
+            this.radioButton3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(320, 167);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Cliker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ AutoCursorCliker/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoCursorCliker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0X20000;
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Location = new Point(0,0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Click_Mouse();
+        }
+        private void Click_Mouse()
+        {
+            int c = 0;
+            POINT p = new POINT();
+            if (radioButton1.Checked)
+            {
+                Thread.Sleep(Convert.ToInt32(textBox3.Text)*1000);
+                while (true)
+                {
+                    GetCursorPos(ref p);
+                    ClientToScreen(Handle, ref p);
+                    DoMouseLeftClick(p.x, p.y); // Ліва кнопка миши
+                    c++;
+                    Thread.Sleep(Convert.ToInt32(textBox2.Text));
+                    if (c == Convert.ToInt32(textBox1.Text))
+                    {
+                        break;
+                    }
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                Thread.Sleep(Convert.ToInt32(textBox3.Text)*1000);
+                while (true)
+                {
+                    GetCursorPos(ref p);
+                    ClientToScreen(Handle, ref p);
+                    DoMouseRightClick(p.x, p.y); // права кнопка миши
+                    c++;
+                    Thread.Sleep(Convert.ToInt32(textBox2.Text));
+                    if (c == Convert.ToInt32(textBox1.Text))
+                    {
+                        break;
+                    }
+                }
+            }
+            else if (radioButton3.Checked)
+            {
+                Thread.Sleep(Convert.ToInt32(textBox3.Text)*1000);
+                while (true)
+                {
+                    GetCursorPos(ref p);
+                    ClientToScreen(Handle, ref p);
+                    DoMouseDoubleLeftClick(p.x, p.y); // подвійний клік лівою кнопкою миши
+                    c++;
+                    Thread.Sleep(Convert.ToInt32(textBox2.Text));
+                    if (c == Convert.ToInt32(textBox1.Text))
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Оберіть кнопку яку хочете використовувати, будь-ласка","Увага", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+            //Thread.Sleep(Convert.ToInt32(textBox3.Text));
+            //while (true)
+            //{
+            //    GetCursorPos(ref p);
+            //    ClientToScreen(Handle, ref p);
+            //    //DoMouseLeftClick(p.x, p.y); // Ліва кнопка миши
+            //    //DoMouseRightClick(p.x, p.y); // права кнопка миши
+            //    //DoMouseDoubleLeftClick(p.x, p.y); // подвійний клік лівою кнопкою миши
+            //    c++;
+            //    Thread.Sleep(Convert.ToInt32(textBox2.Text));
+            //    if (c == Convert.ToInt32(textBox1.Text))
+            //    {
+            //        break;
+            //    }
+            //}
+
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool ClientToScreen(IntPtr hWnd, ref POINT point);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dsFlags, int dx, int dy, int cButtons, int dsExtraInfo);
+
+        public const int MOUSE_EVENT_F_LEFTDOWN = 0X02;
+        public const int MOUSE_EVENT_F_LEFTUP = 0X04;
+
+        public const int MOUSE_EVENT_F_RIGHTDOWN = 0x08;
+        public const int MOUSE_EVENT_F_RIGHTUP = 0x10;
+
+        private void DoMouseLeftClick(int x, int y)
+        {
+            mouse_event(MOUSE_EVENT_F_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSE_EVENT_F_LEFTUP, x, y, 0, 0);
+        }
+        private void DoMouseRightClick(int x, int y)
+        {
+            mouse_event(MOUSE_EVENT_F_RIGHTDOWN, x, y, 0, 0);
+            mouse_event(MOUSE_EVENT_F_RIGHTUP, x, y, 0, 0);
+        }
+        private void DoMouseDoubleLeftClick(int x, int y)
+        {
+            mouse_event(MOUSE_EVENT_F_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSE_EVENT_F_LEFTUP, x, y, 0, 0);
+
+            mouse_event(MOUSE_EVENT_F_LEFTDOWN, x, y, 0, 0);
+            mouse_event(MOUSE_EVENT_F_LEFTUP, x, y, 0, 0);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(ref POINT lpPoint);
+    }
+}
